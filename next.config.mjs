745acddaa1ce68c2651ce89461f5d7af166bfdfc
Zayn <123386073+Zaynsky12@users.noreply.github.com>
2026-06-01 07:31:54 +0100

    fix: rename next config to mjs and ignore eslint during builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,19 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: '/(.*)',
         headers: [
           {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
           },
         ],
       },
